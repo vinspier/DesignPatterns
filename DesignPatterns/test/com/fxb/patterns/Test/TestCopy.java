55e184c7d886e4dcfaf984e80bf95443e37132e6
vinspier <17767160232@163.com>
2019-02-14 20:49:08 +0800

    创建 测试clone的 深复制与浅复制 例子

---
+++ DesignPatterns/test/com/fxb/patterns/Test/TestCopy.java
@@ -0,0 +1,46 @@
+package com.fxb.patterns.Test;
+
+import java.util.ArrayList;
+
+public class TestCopy implements Cloneable{
+    private String nane;
+    private ArrayList<String> list = new ArrayList<>();
+
+    public static void main(String[] args) {
+        TestCopy t = new TestCopy();
+        t.setNane("abc");
+        t.add("a").add("b").add("c");
+        System.out.println(t.getNane());
+        System.out.println(t.getList().size());
+        System.out.println("************original*******************");
+        try {
+            TestCopy tc = (TestCopy)t.clone();
+            tc.add("d");
+            tc.setNane("cba");
+            System.out.println(tc.getNane());
+            System.out.println(tc.getList().size());
+            System.out.println("************after change*******************");
+            System.out.println(t.getNane());
+            System.out.println(t.getList().size());
+        } catch (CloneNotSupportedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public TestCopy add(String s){
+        list.add(s);
+        return this;
+    }
+
+    public String getNane() {
+        return nane;
+    }
+
+    public void setNane(String nane) {
+        this.nane = nane;
+    }
+
+    public ArrayList<String> getList() {
+        return list;
+    }
+}
