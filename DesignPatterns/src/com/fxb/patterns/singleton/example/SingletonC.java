a2d086973fb953760b8b6433f42c41f06503969a
fxb <fanxb@haibaobaoxian.cn>
2020-11-18 18:04:37 +0800

    修改 单例 double check 变量通过volatile修饰

--- DesignPatterns/src/com/fxb/patterns/singleton/example/SingletonC.java
+++ DesignPatterns/src/com/fxb/patterns/singleton/example/SingletonC.java
@@ -9,8 +9,8 @@ public class SingletonC {
      * 定义成静态类变量 当第一次访问该变量时 促使类被加载并初始化该变量
      * final 需要时可以静止指令重排序 在定义是必须被赋值
      * */
-    // private static volatile SingletonC instance;
-    private static SingletonC instance;
+     private static volatile SingletonC instance;
+    //private static SingletonC instance;
 
     /** 构造器私有化 禁止除了以下的入口外其他方法产生实例 */
     private SingletonC() {
