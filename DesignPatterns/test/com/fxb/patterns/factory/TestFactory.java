eaba4b7dd64512df4d4bf4ba20ca5fbaa59425d0
fanxiaobiao <xiaobiao.fan@hibobi.com>
2021-09-27 09:59:58 +0800

    修改 工厂模式

--- DesignPatterns/test/com/fxb/patterns/factory/TestFactory.java
+++ DesignPatterns/test/com/fxb/patterns/factory/TestFactory.java
@@ -1,21 +1,18 @@
 package com.fxb.patterns.factory;
 
+import com.alibaba.fastjson.JSONObject;
 import com.fxb.patterns.factory.example.*;
 
 public class TestFactory {
     public static void main(String[] args) {
         Factory factoryA = new ConcreteFactoryA();
         ProductA bpA = (ProductA) factoryA.create();
-        System.out.println(bpA.getName());
-        System.out.println(bpA.getCode());
-        System.out.println(bpA.getCreateTime());
-        System.out.println(bpA.getSpecialField());
+        System.out.println(JSONObject.toJSONString(bpA));
+
         System.out.println("--------------------------------------------");
+
         Factory factoryB = new ConcreteFactoryB();
         ProductB bpB = (ProductB) factoryB.create();
-        System.out.println(bpB.getName());
-        System.out.println(bpB.getCode());
-        System.out.println(bpB.getCreateTime());
-        System.out.println(bpB.getSpecialField());
+        System.out.println(JSONObject.toJSONString(bpB));
     }
 }
